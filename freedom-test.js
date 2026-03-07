39f188aa5783b64e3784a0fdce7e7d2d9c88315c
baurybot-hub <baurybot@googlemail.com>
2026-03-07 13:07:11 +0100

    Update freedom-test.js

--- freedom-test.js
+++ freedom-test.js
@@ -1,5 +1,5 @@
-import 'dotenv/config';
-import { createClient } from '@supabase/supabase-js';
+require('dotenv/config');
+const { createClient } = require('@supabase/supabase-js');
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
